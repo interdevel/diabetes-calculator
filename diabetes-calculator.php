e9de9ede338514bd13161e3e22a9a62f683786dd
Luis Molina <interdevel@gmail.com>
2024-11-02 12:28:09 +0100

    Added: diabetes-calculator main file

---
+++ diabetes-calculator.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:       Diabetes Calculator
+ * Plugin URI:        https://github.com/interdevel/diabetes-calculator/
+ * Description:       A WordPress plugin to help with diabetes care.
+ * Version:           0.0.1
+ * Requires at least: 5.3
+ * Requires PHP:      7.4
+ * Author:            Luis Molina
+ * Author URI:        https://github.com/interdevel
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       diabetes-calculator
+ * Domain Path:       /languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
+
